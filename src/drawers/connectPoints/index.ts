4b215a26e03c1bbd246e6207f5fc0c0223048857
bojan.cognata <bojan@cognata.com>
2023-07-06 13:51:30 +0200

    Left file

--- src/drawers/connectPoints/index.ts
+++ src/drawers/connectPoints/index.ts
@@ -1,2 +1,8 @@
 export * from './ConnectPointDrawer';
 export * from './ConnectPointsDrawer';
+export * from './EventIconDrawer';
+export * from './InputIconDrawer';
+export * from './OutputIconDrawer';
+export * from './CheckIconDrawer';
+export * from './CloseIconDrawer';
+
