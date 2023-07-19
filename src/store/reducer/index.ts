bf81d9b93f30f941f4ec20a62c0c31a06ddcc9cd
bojan.cognata <bojan@cognata.com>
2023-07-19 14:43:48 +0200

    Style refactor

--- src/store/reducer/index.ts
+++ src/store/reducer/index.ts
@@ -1,31 +1,3 @@
-import {
-	startConnectLineDrawReducer,
-	StartConnectLineDrawAction,
-	MoveConnectLineDrawAction,
-	moveConnectLineDrawReducer,
-	linkConnectLineDrawReducer,
-	LinkConnectLineDrawAction,
-	deleteConnectLineDrawReducer,
-} from './createConnectLineReducer';
-import {
-	PinConnectLineAction,
-	pinConnectLineReducer,
-	unpinConnectLineReducer,
-} from './connectPointReducer';
-
-export {
-	startConnectLineDrawReducer,
-	moveConnectLineDrawReducer,
-	linkConnectLineDrawReducer,
-	deleteConnectLineDrawReducer,
-	pinConnectLineReducer,
-	unpinConnectLineReducer,
-};
-
-export type {
-	StartConnectLineDrawAction,
-	MoveConnectLineDrawAction,
-	LinkConnectLineDrawAction,
-	PinConnectLineAction,
-};
-
+export * from './createConnectLineReducer';
+export * from './connectPointReducer';
+export * from './elementReducer';
