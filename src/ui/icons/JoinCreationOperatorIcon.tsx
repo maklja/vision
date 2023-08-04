f3a6da47ff784ade78272e7fbfede0285be62a5e
bojan.cognata <bojan@cognata.com>
2023-08-04 09:25:43 +0200

    Add transformation operator group icon

--- src/ui/icons/JoinCreationOperatorIcon.tsx
+++ src/ui/icons/JoinCreationOperatorIcon.tsx
@@ -4,7 +4,7 @@ import { SvgIcon } from './SvgIcon';
 export const JoinCreationOperatorIcon = (props: SvgIconProps) => {
 	return (
 		<SvgIcon {...props}>
-			<g transform="translate(1.5 1.5) scale(1.3 1.3) rotate(90, 8, 8)">
+			<g strokeWidth={1.25} transform="translate(1.5 1.5) scale(1.3 1.3) rotate(90, 8, 8)">
 				<path d="M8.5.134a1 1 0 0 0-1 0l-6 3.577a1 1 0 0 0-.5.866v6.846a1 1 0 0 0 .5.866l6 3.577a1 1 0 0 0 1 0l6-3.577a1 1 0 0 0 .5-.866V4.577a1 1 0 0 0-.5-.866L8.5.134z" />
 			</g>
 		</SvgIcon>
--- src/ui/icons/SubscriberOperatorIcon.tsx
+++ src/ui/icons/SubscriberOperatorIcon.tsx
@@ -4,12 +4,18 @@ import { SvgIcon } from './SvgIcon';
 export const SubscriberOperatorIcon = (props: SvgIconProps) => {
 	return (
 		<SvgIcon {...props}>
-			<g transform="translate(4 4)">
-				<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14z" />
-			</g>
+			<g strokeWidth={1.25} transform="scale(1.3 1.3) translate(1.5 1.5)">
+				<g>
+					<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14z" />
+				</g>
 
-			<g transform="scale(0.7 0.7) translate(9 9) ">
-				<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14z" />
+				<g
+					fill="currentColor"
+					stroke="none"
+					transform="scale(0.65 0.65) translate(4.2 4.2)"
+				>
+					<path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14z" />
+				</g>
 			</g>
 		</SvgIcon>
 	);
