67752c71df4114e5dfd0c42390637fcb491c19f1
bojan.cognata <bojan@cognata.com>
2023-08-21 20:50:17 +0200

    Intersection

--- src/drawers/connectLine/ConnectLineDrawer.tsx
+++ src/drawers/connectLine/ConnectLineDrawer.tsx
@@ -2,6 +2,7 @@ import { Line, Group } from 'react-konva';
 import { Point } from '../../model';
 import { Theme } from '../../theme';
 import { ConnectLineArrow } from './ConnectLineArrow';
+import { useMemo } from 'react';
 
 export interface ConnectLineDrawerProps {
 	id: string;
@@ -10,24 +11,20 @@ export interface ConnectLineDrawerProps {
 	visible?: boolean;
 }
 
-// float dx = x1 - x0; //delta x
-// float dy = y1 - y0; //delta y
-// float linelength = sqrtf(dx * dx + dy * dy);
-// dx /= linelength;
-// dy /= linelength;
-// //Ok, (dx, dy) is now a unit vector pointing in the direction of the line
-// //A perpendicular vector is given by (-dy, dx)
-// const float thickness = 5.0f; //Some number
-// const float px = 0.5f * thickness * (-dy); //perpendicular vector with lenght thickness * 0.5
-// const float py = 0.5f * thickness * dx;
-// glBegin(GL_QUADS);
-// glVertex2f(x0 + px, y0 + py);
-// glVertex2f(x1 + px, y1 + py);
-// glVertex2f(x1 - px, y1 - py);
-// glVertex2f(x0 - px, y0 - py);
-// glEnd();
+const findIntersection = (p0: Point, p1: Point, p2: Point, p3: Point): Point => {
+	const slope1 = (p1.y - p0.y) / (p1.x - p0.x);
+	const yIntercept1 = p0.y - slope1 * p0.x;
 
-const createPolygon = (p0: Point, p1: Point, thickness = 5): Point[] => {
+	const slope2 = (p3.y - p2.y) / (p3.x - p2.x);
+	const yIntercept2 = p2.y - slope2 * p2.x;
+
+	const x = (yIntercept2 - yIntercept1) / (slope1 - slope2);
+	const y = slope1 * x + yIntercept1;
+
+	return { x, y };
+};
+
+const createPolygon = (p0: Point, p1: Point, thickness = 15): Point[] => {
 	const dx = p1.x - p0.x;
 	const dy = p1.y - p0.y;
 	const lineLength = Math.sqrt(dx * dx + dy * dy);
@@ -35,17 +32,59 @@ const createPolygon = (p0: Point, p1: Point, thickness = 5): Point[] => {
 	const directionX = dx / lineLength;
 	const directionY = dy / lineLength;
 
-	const px = 0.5 * thickness * -directionX;
-	const py = 0.5 * thickness * directionY;
+	const px = 0.5 * thickness * directionY * -1;
+	const py = 0.5 * thickness * directionX;
 
-	return [];
+	return [
+		{
+			x: p0.x + px,
+			y: p0.y + py,
+		},
+		{
+			x: p1.x + px,
+			y: p1.y + py,
+		},
+		{
+			x: p1.x - px,
+			y: p1.y - py,
+		},
+		{
+			x: p0.x - px,
+			y: p0.y - py,
+		},
+	];
 };
 
 export const ConnectLineDrawer = ({ points, theme, visible = true }: ConnectLineDrawerProps) => {
 	const drawAnArrow = points.length > 3;
 
+	const boundingBoxPoints = useMemo(() => {
+		const groupedPoints: [Point, Point][] = points.reduce(
+			(groups: [Point, Point][], point, i) => {
+				const nextPoint = points[i + 1];
+				if (!nextPoint) {
+					return groups;
+				}
+
+				return [...groups, [point, nextPoint]];
+			},
+			[],
+		);
+
+		return groupedPoints.map(([p0, p1]) => createPolygon(p0, p1).flatMap((p) => [p.x, p.y]));
+	}, [points]);
+
 	return (
 		<Group visible={visible}>
+			{boundingBoxPoints.map((points, i) => (
+				<Line
+					{...theme.connectLine.line}
+					key={i}
+					perfectDrawEnabled={false}
+					points={points}
+				/>
+			))}
+
 			<Line
 				{...theme.connectLine.line}
 				perfectDrawEnabled={false}
@@ -55,3 +94,4 @@ export const ConnectLineDrawer = ({ points, theme, visible = true }: ConnectLine
 		</Group>
 	);
 };
+
