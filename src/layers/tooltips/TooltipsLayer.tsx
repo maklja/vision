78bf44a8610bd34253ecbcbbb9f8ab49972adeed
Bojan Makljenovic <maklja90@gmail.com>
2023-11-30 07:24:03 +0100

    Tooltip update

--- src/layers/tooltips/TooltipsLayer.tsx
+++ src/layers/tooltips/TooltipsLayer.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { Layer } from 'react-konva';
 import { useAppSelector } from '../../store/rootState';
 import { selectElementSizeOptions, useBoundingBox, useThemeContext } from '../../store/stageSlice';
@@ -6,6 +7,8 @@ import { selectStageElementById } from '../../store/elements';
 import { selectTooltip } from '../../store/tooltip';
 import { selectElementErrorById } from '../../store/errors';
 
+const TOOLTIP_SHOW_TIME = 1_000;
+
 export const TooltipsLayer = () => {
 	const theme = useThemeContext();
 	const tooltip = useAppSelector(selectTooltip);
@@ -13,7 +16,23 @@ export const TooltipsLayer = () => {
 	const elementSizeOptions = useAppSelector(selectElementSizeOptions);
 	const bb = useBoundingBox(element?.type ?? null, { x: element?.x ?? 0, y: element?.y ?? 0 });
 	const error = useAppSelector(selectElementErrorById(element?.id ?? null));
-	const text = error?.errorMessage ?? tooltip?.text;
+	const text = error?.errorMessage ?? tooltip?.text ?? element?.name;
+
+	const [visible, setVisible] = useState(false);
+
+	useEffect(() => {
+		if (!element?.visible) {
+			return;
+		}
+
+		const timeoutId = setTimeout(() => setVisible(true), TOOLTIP_SHOW_TIME);
+
+		return () => {
+			clearTimeout(timeoutId);
+			setVisible(false);
+		};
+	}, [element]);
+
 	return (
 		<Layer>
 			{text && element && tooltip ? (
@@ -25,10 +44,9 @@ export const TooltipsLayer = () => {
 					x={bb.center.x}
 					y={bb.topLeft.y}
 					width={Math.max(bb.width, 150)}
-					visible={element.visible}
+					visible={visible}
 				/>
 			) : null}
 		</Layer>
 	);
 };
-
