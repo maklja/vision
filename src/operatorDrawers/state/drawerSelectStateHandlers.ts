d9f6dc688e26dd21e4a1f1f4a0326ae4b171307d
Bojan Makljenovic <39374578+maklja@users.noreply.github.com>
2024-03-25 14:04:09 +0100

    Add stage edge auto drag support (#38)

--- src/operatorDrawers/state/drawerSelectStateHandlers.ts
+++ src/operatorDrawers/state/drawerSelectStateHandlers.ts
@@ -17,59 +17,61 @@ import {
 import { changeCursorStyle } from '../utils';
 import { drawerAnimationStateHandlers } from './drawerAnimationStateHandlers';
 
-export const drawerSelectStateHandlers = (dispatch: AppDispatch): DrawerEvents => ({
-	...drawerAnimationStateHandlers,
-	onMouseDown: (e: DrawerEvent) => {
-		if (e.originalEvent) {
-			e.originalEvent.cancelBubble = true;
-		}
+export function drawerSelectStateHandlers(dispatch: AppDispatch): DrawerEvents {
+	return {
+		...drawerAnimationStateHandlers,
+		onMouseDown: (e: DrawerEvent) => {
+			if (e.originalEvent) {
+				e.originalEvent.cancelBubble = true;
+			}
 
-		if (e.originalEvent?.evt.ctrlKey) {
-			dispatch(toggleSelectElement({ id: e.id }));
-			dispatch(toggleSelectionConnectPoint({ elementId: e.id }));
-		} else {
-			// here order matters so be aware that connect points are checking if
-			// element that they belong to is selected as well
-			dispatch(selectConnectPoint({ elementId: e.id }));
-			dispatch(selectElement({ id: e.id }));
-		}
-	},
-	onMouseUp: (e: DrawerEvent) => {
-		if (e.originalEvent?.evt.ctrlKey) {
-			return;
-		}
+			if (e.originalEvent?.evt.ctrlKey) {
+				dispatch(toggleSelectElement({ id: e.id }));
+				dispatch(toggleSelectionConnectPoint({ elementId: e.id }));
+			} else {
+				// here order matters so be aware that connect points are checking if
+				// element that they belong to is selected as well
+				dispatch(selectConnectPoint({ elementId: e.id }));
+				dispatch(selectElement({ id: e.id }));
+			}
+		},
+		onMouseUp: (e: DrawerEvent) => {
+			if (e.originalEvent?.evt.ctrlKey) {
+				return;
+			}
 
-		dispatch(clearSelected());
-		dispatch(selectElements([{ id: e.id }]));
-		dispatch(setSelectionConnectPoints({ elementIds: [e.id] }));
-	},
-	onMouseOver: (e: DrawerEvent) => {
-		const { id, originalEvent } = e;
-		if (originalEvent) {
-			originalEvent.cancelBubble = true;
-			changeCursorStyle('pointer', originalEvent.currentTarget.getStage());
-			dispatch(highlight([{ id }]));
-			dispatch(
-				showTooltip({
-					elementId: id,
-				}),
-			);
-		}
-	},
-	onMouseOut: (e: DrawerEvent) => {
-		const { originalEvent } = e;
-		if (originalEvent) {
-			originalEvent.cancelBubble = true;
-			changeCursorStyle('default', originalEvent.currentTarget.getStage());
-			dispatch(highlight([]));
-			dispatch(hideTooltip());
-		}
-	},
-	onDragStart: (e: DrawerEvent) => {
-		if (e.originalEvent) {
-			e.originalEvent.cancelBubble = true;
-		}
-		dispatch(changeState(StageState.Dragging));
-	},
-});
+			dispatch(clearSelected());
+			dispatch(selectElements([{ id: e.id }]));
+			dispatch(setSelectionConnectPoints({ elementIds: [e.id] }));
+		},
+		onMouseOver: (e: DrawerEvent) => {
+			const { id, originalEvent } = e;
+			if (originalEvent) {
+				originalEvent.cancelBubble = true;
+				changeCursorStyle('pointer', originalEvent.currentTarget.getStage());
+				dispatch(highlight([{ id }]));
+				dispatch(
+					showTooltip({
+						elementId: id,
+					}),
+				);
+			}
+		},
+		onMouseOut: (e: DrawerEvent) => {
+			const { originalEvent } = e;
+			if (originalEvent) {
+				originalEvent.cancelBubble = true;
+				changeCursorStyle('default', originalEvent.currentTarget.getStage());
+				dispatch(highlight([]));
+				dispatch(hideTooltip());
+			}
+		},
+		onDragStart: (e: DrawerEvent) => {
+			if (e.originalEvent) {
+				e.originalEvent.cancelBubble = true;
+			}
+			dispatch(changeState(StageState.Dragging));
+		},
+	};
+}
 
